b54bde9f6b894033413cbf5ad27a5e98b5c5cd47
DamianSz2000 <92025938+DamianSz2000@users.noreply.github.com>
2021-11-25 15:09:40 +0100

    Poprawki

--- lista3/zadanie1.m
+++ lista3/zadanie1.m
@@ -1,4 +1,4 @@
-x = [-5:.01:5];
+x = [-5:.001:5];
 f_x = exp(-x.^2);
 subplot(2,2,1)
 plot(x,f_x)
@@ -16,8 +16,8 @@ ylabel('\it{y}','FontSize', 14)
 title('Pochodna a)','FontSize', 16)
 hold off
 subplot(2,2,3)
-f_x3 = diff(f_x)./diff(x)
-f_x3(1001) = 0
+f_x3 = diff(f_x)./(diff(x-0.0005))
+f_x3(10001) = 0
 plot(x, f_x3)
 xlabel('\it{x}','FontSize', 14)
 ylabel('\it{y}','FontSize', 14)
@@ -29,12 +29,12 @@ xlabel('\it{x}','FontSize', 14)
 ylabel('\it{y}','FontSize', 14)
 title('Funkcja \it{f(x) = e^{-x^2}}','FontSize', 16)
 subplot(1,2,2)
-f_x2 = (exp(-(x+0.5).^2) - exp(-(x-0.5).^2))./2.*0.5
-plot(x, f_x2)
+f_x2 = (exp(-(x+0.01).^2) - exp(-(x-0.01).^2))./(2.*0.01)
+plot(x, f_x2,"-o")
 hold on
-plot(x, f_x3)
+plot(x, f_x3,"-o")
 f_x4 = -2.*x.*f_x
-plot(x,f_x4)
+plot(x,f_x4,"-o")
 xlabel('\it{x}','FontSize', 14)
 ylabel('\it{y}','FontSize', 14)
 title('Pochodne na 3 różne sposoby','FontSize', 16)
